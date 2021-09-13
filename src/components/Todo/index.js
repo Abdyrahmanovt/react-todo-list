d0c73bff609863e5b72587c5ca0d2c149aaf1a6f
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2021-09-13 12:20:11 +0600

    removed the error

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrash, faEdit} from '@fortawesome/free-solid-svg-icons'
 import {nanoid} from 'nanoid'
 import TodoItem from "../TodoItem";
 import TodoHeader from "../TodoHeader";
@@ -9,17 +7,23 @@ const Todo = () => {
     const [todos, setTodos] = useState([
         {
             id: 1,
-            title: 'Выпить весь чай'
+            title: 'Выпить весь чай',createdAt:1631425895683
         },
         {
             id: 2,
-            title: 'Собраться на работу'
+            title: 'Собраться на работу',createdAt:1630907495683
+        },
+        {
+           id:3,
+           title:'Ничего не делать',createdAt:1630648295683
         }
     ])
+    const [filtered,setFiltered] = useState(todos)
     const [value, setValue] = useState('')
     const handleInput = (e) => {
         setValue(e.target.value)
     }
+    
     const addTodo = () => {
         const newTodo = {
             id: nanoid(),    //todos[todos.length] ?todos[todos.length - 1].id +1  : 1
@@ -32,19 +36,30 @@ const Todo = () => {
         setTodos(todos.filter(item => item.id !== id))
     }
 
-    const saveTodo = (id, title) => {
+    const saveTodo = (id, title) =>     {
         setTodos(todos.map(item => item.id === id ? {...item , title} : item ))
+    }
+
+
+    const filterByDate = (type)=>{
+        const times = {
+            day: 24 * 60 * 60 * 1000,
+            week: 24 * 60 * 60 * 1000 * 7,
+            month: 24 * 60 * 60 * 1000 * 30,
+            all: +new Date()
+        }
+        setFiltered(todos.filter(item => +new Date() - item.createdAt < times[type]))
     }
 
     return (
         <div className='row my-5 '>
             <div className="col-md-4 offset-md-4">
-                <TodoHeader value={value} handleInput={handleInput} addTodo={addTodo} length={todos.length}/>
+                <TodoHeader value={value} handleInput={handleInput} addTodo={addTodo} length={todos.length} filterByDate={filterByDate}/>
                 <button type='button' className='btn btn-danger mt-3 w-100' onClick={()=> setTodos([])}>Clear all</button>
                 <ul className='list-group'>
                     {
-                        todos.map(item =>
-                            <TodoItem item={item}  deleteTodo={deleteTodo} saveTodo={saveTodo}/>
+                        filtered.map(item =>
+                            <TodoItem item={item}  deleteTodo={deleteTodo} saveTodo={saveTodo} />
                         )
                     }
                 </ul>
--- src/components/TodoHeader/index.js
+++ src/components/TodoHeader/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const TodoHeader = ({value, addTodo,handleInput,length}) => {
+const TodoHeader = ({value, addTodo,handleInput,length,filterByDate}) => {
     return (
       <div>
           <div className="d-flex align-items-center justify-content-between">
               <h1>TodoList</h1>
               <span>item: {length}</span>
+          </div>
+          <div className="d-flex align-items-center">
+              <button type="button" className="btn btn-sm btn-secondary mb-2 me-2 " onClick={() => filterByDate("all")}>All</button>
+              <button type="button" className="btn btn-sm btn-secondary mb-2 me-2 " onClick={() => filterByDate("day")} >day</button>
+              <button type="button" className="btn btn-sm btn-secondary mb-2 me-2 " onClick={() => filterByDate("week")}>week</button>
+              <button type="button" className="btn btn-sm btn-secondary mb-2" onClick={() => filterByDate("month")} >month</button>
           </div>
           <div className="d-flex mb-4">
               <input type="text" className='form-control me-2' value={value} onChange={handleInput}/>
